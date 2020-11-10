a76e7a25cf0e2f737c55c030ee2e929f387e65a3
frezcirno <2277861660@qq.com>
2020-11-11 01:11:00 +0800

    temp

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,76 +12,102 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    var firstNumberText = ""
-    var secondNumberText = ""
-    var op = ""
-    var isFirstNumber = true
-    var hasOp = false
-    var canClear = true
+    var firstNumberText: String?
+    var secondNumberText: String?
+    var op: String?
+    var finish = true
+    var wait = 1
     
     enum Operation {
-        case Unary(String, (Double)->Double)
-        case Binary(String, (Double, Double)->Double)
-    }
-    
-    private var knownOps = [
-        "+":Operation.Binary("+", +),
-        "-":Operation.Binary("−"){ $1 - $0 },
-        "*":Operation.Binary("*", *),
-        "/":Operation.Binary("/"){ $1 / $0 }
-    ]
-    
-    @IBAction func handleButtonPress(_ sender: UIButton) {
-        if canClear {
-            resultLabel.text = ""
-            canClear = false
-        }
-        let t = resultLabel.text!
-        if let text = sender.titleLabel?.text {
-            switch text {
-            case "+", "*", "/", "-":
-                if hasOp {
-                    return
-                }
-                op = text
-                isFirstNumber = false
-                hasOp = true
-                resultLabel.text = "\(t)\(op)"
-                break
-            case "=":
-                isFirstNumber = true
-                hasOp = false
-                canClear = true
-                let result = calculate()
-                resultLabel.text = "\(result)"
-                break
-            case "CE":
-                _ = resultLabel.text?.popLast()
-            case "C":
-                resultLabel.text = nil
-            default:
-                if isFirstNumber {
-                    firstNumberText = "\(firstNumberText)\(text)"
-                } else {
-                    secondNumberText = "\(secondNumberText)\(text)"
-                }
-                resultLabel.text = "\(t)\(text)"
-                break;
+        case Unary((Double)->Double)
+        case Binary((Double, Double)->Double)
+        func apply(_ arg1: Double?, _ arg2: Double?)->Double{
+            switch self {
+            case let .Unary(op):
+                return op(arg1!)
+            case let .Binary(op):
+                return op(arg1!, arg2!)
             }
         }
     }
     
-    func calculate<T:NSNumber>(_ firstNumber:T, _ secondNumber:T, op: (T,T)->T) -> T {
-        return op(firstNumber, secondNumber)
+    private var knownOps = [
+        "+": Operation.Binary(+),
+        "-": Operation.Binary(){ $1 - $0 },
+        "*": Operation.Binary(*),
+        "/": Operation.Binary(){ $1 / $0 },
+        "n": Operation.Unary(){ -$0 },
+        "p": Operation.Unary(){ $0 }
+    ]
+    
+    func reset() {
+        op = nil
+        firstNumberText = nil
+        secondNumberText = nil
+        finish = true
+        wait = 1
+    }
+    
+    class State {
+        var display: String = "";
+        
+        func transform(_ next: String) -> State {
+            display += next
+            return self
+        }
     }
     
     
-    func add<T:NSNumber>(_ n1:T,_ n2:T) -> T { return n1+n2 }
-    func sub<T:NSNumber>(_ n1:T,_ n2:T) -> T { return n1-n2 }
-    func mul<T:NSNumber>(_ n1:T,_ n2:T) -> T { return n1*n2 }
-    func div<T:NSNumber>(_ n1:T,_ n2:T) -> T { return n1/n2 }
-    
-    
-    
+    @IBAction func handleButtonPress(_ sender: UIButton) {
+        if let token = sender.titleLabel?.text {
+            var state = State()
+            state.transform("H").transform("e")
+            if finish {
+                resultLabel.text?.removeAll()
+                finish = false
+            }
+            switch token {
+            case "CE":
+                _ = resultLabel.text?.popLast()
+            case "C":
+                resultLabel.text?.removeAll()
+                reset()
+            case "+", "*", "/", "-":
+                if op != nil {
+                    _ = resultLabel.text?.popLast()
+                    return
+                } else {
+                    op = token
+                }
+                resultLabel.text?.append(token)
+                wait = 2
+                break
+            case "=":
+                guard (op != nil), (firstNumberText != nil), (secondNumberText != nil) else {
+                    return
+                }
+                let result = knownOps[op!]!.apply(Double(firstNumberText!)!, Double(secondNumberText!))
+                resultLabel.text = "\(result)"
+                reset()
+                break
+            default: // 0123456789.
+                if wait == 1 {
+                    if firstNumberText == nil {
+                        firstNumberText = token
+                    } else {
+                        firstNumberText?.append(token)
+                    }
+                } else if wait == 2 {
+                    if secondNumberText == nil {
+                        secondNumberText = token
+                    } else {
+                        secondNumberText?.append(token)
+                    }
+                }
+                resultLabel.text?.append(token)
+                break;
+            }
+        }
+    }
 }
 
